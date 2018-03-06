144fa952b9a72867abaa0a70a6434162c78dfd3d
Paul Connolly <p.connolly@manchester.ac.uk>
2018-03-06 13:00:13 +0000

    removed additional comma on line 8 of exportImagesDriver.m

--- matlab/exportImagesDriver.m
+++ matlab/exportImagesDriver.m
@@ -5,7 +5,7 @@ load cmap;
     
 % Exporting all images ++++++++++++++++++++++++++++++++++++++++++++++++++++
 disp('exporting all images...');
-exportImages([path1],filename1,,foc_crit,min_len,MAP2);
+exportImages([path1],filename1,foc_crit,min_len,MAP2);
 disp('done');
 %--------------------------------------------------------------------------
     
