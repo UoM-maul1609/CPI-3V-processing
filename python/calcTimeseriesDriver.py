621520774fc4aeea6fd4ec86d78b73b712859f92
Paul Connolly <p.connolly@manchester.ac.uk>
2018-03-20 21:46:42 +0000

    added code when there is no data for time-series

--- python/calcTimeseriesDriver.py
+++ python/calcTimeseriesDriver.py
@@ -71,66 +71,71 @@ def calcTimeseriesDriver(path1,filename1,foc_crit,dt,ds,vel,outputfile):
                       
         ind,=np.where((ROI_N['imageType'][0,0][:,0] == 89) & \
                       (dat['foc'][0,0]['focus'][0,:] >foc_crit))
-        tmin=np.min(ROI_N['Time'][0,0][ind,0])
-        tmax=np.max(ROI_N['Time'][0,0][ind,0])
-        ilow,=np.where(timeser['Time']>=tmin)
-        ilow=ilow[0]
-        ihigh,=np.where(timeser['Time']<tmax)
-        ihigh=ihigh[-1]
         
-        
-        
-        
-        #=======time loop======================================================
-        f=interp1d(IMAGE1['Time1'][0,0][:,0],IMAGE1['Time'][0,0][0,:].T,kind='nearest',fill_value='extrapolate')
-        for j in range(ilow,ihigh+1):
-            # dead-time: find imge data that are in the time-window
-                        # find corresponding funny time of tiem-windown
-                        # add up dead-times
-            indim,=np.where((IMAGE1['Time1'][0,0][:,0]>=(timeser['Time'][j]-dt2/2.)) & \
-                      (IMAGE1['Time1'][0,0][:,0]<(timeser['Time'][j]+dt2/2.)))
-            # interpolation:
-            twin=f(np.array([timeser['Time'][j]-dt2/2., timeser['Time'][j]+dt2/2.]))
-            indho,=np.where((HOUSE['Time'][0,0][0,:]  >=twin[0]) & \
-                            (HOUSE['Time'][0,0][0,:]< twin[1]))
-            timeser['deadtimes'][j]=np.nansum(HOUSE['deadtime'][0,0][indho,0])
-    
-            timeser['nimages'][j]=len(indim)
+        if(len(ind)):
+            tmin=np.min(ROI_N['Time'][0,0][ind,0])
+            tmax=np.max(ROI_N['Time'][0,0][ind,0])
+            ilow,=np.where(timeser['Time']>=tmin)
+            ilow=ilow[0]
+            ihigh,=np.where(timeser['Time']<tmax)
+            ihigh=ihigh[-1]
             
             
             
-            # particles:
-            ind2,=np.where((dat['Time'][0,0][0,ind]>=(timeser['Time'][j]-dt2/2.)) & \
-                (dat['Time'][0,0][0,ind]<(timeser['Time'][j]+dt2/2)))
-            timeser['conc'][j]=timeser['conc'][j]+len(ind2)
             
+            #=======time loop======================================================
+            f=interp1d(IMAGE1['Time1'][0,0][:,0], \
+                       IMAGE1['Time'][0,0][0,:].T, \
+                       kind='nearest',fill_value='extrapolate')
             
-            
-            # size bins========================================================
-            (N,X)=np.histogram(dat['len'][0,0][ind[ind2],0],size1a)
-            timeser['conc2'][j,:]=timeser['conc2'][j,:]+N
-            #------------------------------------------------------------------
-            
-            # size and roundness bins==========================================
-            for k in range(0,na):
-                ind3=ind[ind2]
-                ind4,=np.where((dat['round'][0,0][ind3,0]>= timeser['ar1'][k]) & \
-                    (dat['round'][0,0][ind3,0]< timeser['ar2'][k]) )
-                (N,X)=np.histogram(dat['len'][0,0][ind3[ind4],0],size1a)
-                timeser['conc2ar'][j,:,k]=timeser['conc2ar'][j,:,k]+N     
-            #------------------------------------------------------------------
-            
-        #----------------------------------------------------------------------
+            for j in range(ilow,ihigh+1):
+                # dead-time: find imge data that are in the time-window
+                            # find corresponding funny time of tiem-windown
+                            # add up dead-times
+                indim,=np.where((IMAGE1['Time1'][0,0][:,0]>=(timeser['Time'][j]-dt2/2.)) & \
+                          (IMAGE1['Time1'][0,0][:,0]<(timeser['Time'][j]+dt2/2.)))
+                # interpolation:
+                twin=f(np.array([timeser['Time'][j]-dt2/2., timeser['Time'][j]+dt2/2.]))
+                indho,=np.where((HOUSE['Time'][0,0][0,:]  >=twin[0]) & \
+                                (HOUSE['Time'][0,0][0,:]< twin[1]))
+                timeser['deadtimes'][j]=np.nansum(HOUSE['deadtime'][0,0][indho,0])
         
-        
-        
-        # scale by sample volume
-        dead=np.transpose(np.tile(timeser['deadtimes'].T,(nl,na,1)), (2,0,1))
-        nimages=np.transpose(np.tile(timeser['nimages'].T,(nl,na,1)), (2,0,1))
-        timeser['conc2ar']=timeser['conc2ar']/((dt-dead)*vel*sa+nimages*sv)
-        
-        timeser['conc2']=np.nansum(timeser['conc2ar'],axis=2)
-        timeser['conc']=np.nansum(timeser['conc2'],axis=1)
+                timeser['nimages'][j]=len(indim)
+                
+                
+                
+                # particles:
+                ind2,=np.where((dat['Time'][0,0][0,ind]>=(timeser['Time'][j]-dt2/2.)) & \
+                    (dat['Time'][0,0][0,ind]<(timeser['Time'][j]+dt2/2)))
+                timeser['conc'][j]=timeser['conc'][j]+len(ind2)
+                
+                
+                
+                # size bins========================================================
+                (N,X)=np.histogram(dat['len'][0,0][ind[ind2],0],size1a)
+                timeser['conc2'][j,:]=timeser['conc2'][j,:]+N
+                #------------------------------------------------------------------
+                
+                # size and roundness bins==========================================
+                for k in range(0,na):
+                    ind3=ind[ind2]
+                    ind4,=np.where((dat['round'][0,0][ind3,0]>= timeser['ar1'][k]) & \
+                        (dat['round'][0,0][ind3,0]< timeser['ar2'][k]) )
+                    (N,X)=np.histogram(dat['len'][0,0][ind3[ind4],0],size1a)
+                    timeser['conc2ar'][j,:,k]=timeser['conc2ar'][j,:,k]+N     
+                #------------------------------------------------------------------
+                
+            #----------------------------------------------------------------------
+            
+            
+            
+            # scale by sample volume
+            dead=np.transpose(np.tile(timeser['deadtimes'].T,(nl,na,1)), (2,0,1))
+            nimages=np.transpose(np.tile(timeser['nimages'].T,(nl,na,1)), (2,0,1))
+            timeser['conc2ar']=timeser['conc2ar']/((dt-dead)*vel*sa+nimages*sv)
+            
+            timeser['conc2']=np.nansum(timeser['conc2ar'],axis=2)
+            timeser['conc']=np.nansum(timeser['conc2'],axis=1)
         
         
         
