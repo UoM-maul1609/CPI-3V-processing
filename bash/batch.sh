d9616563685cb1904371d935c1b0c7fd70f2a23f
Paul Connolly <p.connolly@manchester.ac.uk>
2018-03-23 08:52:19 +0000

    added example batch.sh script

---
+++ bash/batch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+python36 python/batchProcess.py /mnt/mobile4/Database6/PICASSO/rawData/C072/3V-CPI/ #> /mnt/mobile4/Database6/PICASSO/rawData/C072/3V-CPI/python.out &
+python36 python/batchProcess.py /mnt/mobile4/Database6/PICASSO/rawData/C073/3V-CPI/CPI/ #> /mnt/mobile4/Database6/PICASSO/rawData/C073/3V-CPI/CPI/python.out &
+python36 python/batchProcess.py /mnt/mobile4/Database6/PICASSO/rawData/C074/3VCPI/CPI/ #> /mnt/mobile4/Database6/PICASSO/rawData/C074/3VCPI/CPI/python.out &
+python36 python/batchProcess.py /mnt/mobile4/Database6/PICASSO/rawData/C075/3VCPI/3VCPI/CPI/ #> /mnt/mobile4/Database6/PICASSO/rawData/C075/3VCPI/3VCPI/CPI/python.out &
+python36 python/batchProcess.py /mnt/mobile4/Database6/PICASSO/rawData/C076/3VCPI/CPI/ #> /mnt/mobile4/Database6/PICASSO/rawData/C076/3VCPI/CPI/python.out &
+python36 python/batchProcess.py /mnt/mobile4/Database6/PICASSO/rawData/C077/3V-CPI/CPI/ #> /mnt/mobile4/Database6/PICASSO/rawData/C077/3V-CPI/CPI/python.out &
+python36 python/batchProcess.py /mnt/mobile4/Database6/PICASSO/rawData/C078/3VCPI/CPI/ #> /mnt/mobile4/Database6/PICASSO/rawData/C078/3VCPI/CPI/python.out &
+python36 python/batchProcess.py /mnt/mobile4/Database6/PICASSO/rawData/C079/3VCPI/CPI/ #> /mnt/mobile4/Database6/PICASSO/rawData/C079/3VCPI/CPI/python.out &
+python36 python/batchProcess.py /mnt/mobile4/Database6/PICASSO/rawData/C080/3vcpi/CPI/ #> /mnt/mobile4/Database6/PICASSO/rawData/C080/3vcpi/CPI/python.out &
+python36 python/batchProcess.py /mnt/mobile4/Database6/PICASSO/rawData/C081/3VCPI/CPI/ #> /mnt/mobile4/Database6/PICASSO/rawData/C081/3VCPI/CPI/python.out &
+python36 python/batchProcess.py /mnt/mobile4/Database6/PICASSO/rawData/C082/3VCPI/CPI/ #> /mnt/mobile4/Database6/PICASSO/rawData/C082/3VCPI/CPI/python.out &
+
