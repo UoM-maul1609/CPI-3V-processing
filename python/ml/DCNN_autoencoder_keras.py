542f4d1ec329469fc206d04efb5a5ede1382fc3b
Paul Connolly <p.connolly@manchester.ac.uk>
2020-03-23 09:32:50 +0000

    updated autoencoder code

--- python/ml/DCNN_autoencoder_keras.py
+++ python/ml/DCNN_autoencoder_keras.py
@@ -22,62 +22,89 @@ from keras.layers import Dense, Conv2D, \
     Conv2DTranspose, MaxPooling2D, UpSampling2D
 from keras.layers.advanced_activations import LeakyReLU
 from keras.optimizers import adam
+import h5py
 
 
-autoencoder=Sequential()
-# Encoder Layers
-autoencoder.add(Conv2D(32, (3, 3), activation='relu', padding='same', input_shape=(128,128,1)))
-autoencoder.add(MaxPooling2D((2, 2), padding='same'))
-autoencoder.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
-autoencoder.add(MaxPooling2D((2, 2), padding='same'))
-autoencoder.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
-autoencoder.add(MaxPooling2D((2, 2), padding='same'))
-autoencoder.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
-autoencoder.add(MaxPooling2D((2, 2), padding='same'))
-autoencoder.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
-autoencoder.add(MaxPooling2D((2, 2), padding='same'))
-autoencoder.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
-autoencoder.add(MaxPooling2D((2, 2), padding='same'))
-autoencoder.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
-autoencoder.add(MaxPooling2D((2, 2), padding='same'))
-autoencoder.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
+loadData=True
 
-# Flatten encoding for visualization
-autoencoder.add(Flatten())
-autoencoder.add(Reshape((1, 1, 32)))
+defineModel=True
+
+if defineModel:
+    autoencoder=Sequential()
+    # Encoder Layers
+    autoencoder.add(Conv2D(32, (3, 3), activation='relu', padding='same', input_shape=(128,128,1)))
+    autoencoder.add(MaxPooling2D((2, 2), padding='same'))
+    autoencoder.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
+    autoencoder.add(MaxPooling2D((2, 2), padding='same'))
+    autoencoder.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
+    autoencoder.add(MaxPooling2D((2, 2), padding='same'))
+    autoencoder.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
+    autoencoder.add(MaxPooling2D((2, 2), padding='same'))
+    autoencoder.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
+    autoencoder.add(MaxPooling2D((2, 2), padding='same'))
+    autoencoder.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
+    autoencoder.add(MaxPooling2D((2, 2), padding='same'))
+    autoencoder.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
+    autoencoder.add(MaxPooling2D((2, 2), padding='same'))
+    autoencoder.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
+    
+    # Flatten encoding for visualization
+    autoencoder.add(Flatten())
+    autoencoder.add(Reshape((1, 1, 32)))
+    
+    
+    # Decoder Layers
+    autoencoder.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
+    autoencoder.add(UpSampling2D((2, 2)))
+    autoencoder.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
+    autoencoder.add(UpSampling2D((2, 2)))
+    autoencoder.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
+    autoencoder.add(UpSampling2D((2, 2)))
+    autoencoder.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
+    autoencoder.add(UpSampling2D((2, 2)))
+    autoencoder.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
+    autoencoder.add(UpSampling2D((2, 2)))
+    autoencoder.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
+    autoencoder.add(UpSampling2D((2, 2)))
+    autoencoder.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
+    autoencoder.add(UpSampling2D((2, 2)))
+    autoencoder.add(Conv2D(1, (3, 3), activation='sigmoid', padding='same'))
+    
+    autoencoder.summary()
+    
+    autoencoder.compile(optimizer='adam', loss='binary_crossentropy',metrics=['accuracy'])
 
 
-# Decoder Layers
-autoencoder.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
-autoencoder.add(UpSampling2D((2, 2)))
-autoencoder.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
-autoencoder.add(UpSampling2D((2, 2)))
-autoencoder.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
-autoencoder.add(UpSampling2D((2, 2)))
-autoencoder.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
-autoencoder.add(UpSampling2D((2, 2)))
-autoencoder.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
-autoencoder.add(UpSampling2D((2, 2)))
-autoencoder.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
-autoencoder.add(UpSampling2D((2, 2)))
-autoencoder.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
-autoencoder.add(UpSampling2D((2, 2)))
-autoencoder.add(Conv2D(1, (3, 3), activation='sigmoid', padding='same'))
+if loadData:
+    print('Loading data...')
+    # load images
+    h5f = h5py.File('/Users/mccikpc2/Desktop/postProcessed_l50.h5','r')
+    images=h5f['images'][:]
+    lens  =h5f['lens'][:]
+    times =h5f['times'][:]
+    h5f.close()
+    
+    i1 = len(lens)
+    indices = np.random.permutation(i1)
+    split1=int(0.8*i1)
+    training_idx, test_idx = indices[:split1], indices[split1:]
+    
+    x_train, x_test = images[training_idx,:], images[test_idx,:]
+    lens_train,lens_test = lens[training_idx,:], lens[test_idx,:]
+    times_train,times_test = times[training_idx,:], times[test_idx,:]
 
-autoencoder.summary()
-
-autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
+    print('data is loaded')
 
 # train the model
-#autoencoder.fit(x_train, x_train, epochs=100, batch_size=128, \
-#                validation_data=(x_test,x_test))
+autoencoder.fit(x_train, x_train, epochs=100, batch_size=128, \
+                validation_data=(x_test,x_test),verbose=1)
 
 
 # see https://keras.io/getting-started/faq/#how-can-i-obtain-the-output-of-an-intermediate-layer
-#layer_name='flatten_1'
-#intermediate_layer_model = Model(input=autoencoder.input, \
-#                        outputs=autoencoder.get_layer(layer_name).output)
-#
+layer_name='flatten_1'
+intermediate_layer_model = Model(input=autoencoder.input, \
+                        outputs=autoencoder.get_layer(layer_name).output)
+
 #intermediate_output = intermediate_layer_model.predict(data)
 
 
