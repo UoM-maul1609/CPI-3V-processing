965f29bb7aad869a1e5bb9254dd8eff54e72f7ca
Paul Connolly <p.connolly@manchester.ac.uk>
2020-03-25 08:47:49 +0000

    added script for calculating encoding - for cluster analysis

---
+++ python/ml/DCNN_autoencoder_keras_save_encoding.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 22 10:31:14 2020
+
+@author: mccikpc2
+"""
+
+"""
+    Keras deep convolutional autoencoder
+    https://medium.com/analytics-vidhya/building-a-convolutional-autoencoder-using-keras-using-conv2dtranspose-ca403c8d144e
+"""
+
+#import tensorflow
+import keras
+import numpy as np
+import matplotlib.pyplot as plt
+from keras.models import Model, model_from_json
+import h5py
+
+
+loadData=True
+inputs='/models/mccikpc2/CPI-analysis/model_epochs_15_dense64'
+
+
+
+
+if loadData:
+    print('Loading data...')
+    # load images
+    h5f = h5py.File('/models/mccikpc2/CPI-analysis/postProcessed_l50.h5','r')
+    images=h5f['images'][:]
+    images=np.expand_dims(images,axis=3)
+    lens  =h5f['lens'][:]
+    times =h5f['times'][:]
+    h5f.close()
+    
+    print('data is loaded')
+
+
+
+
+"""
+    load the model++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+"""
+print('Loading model...')
+json_file = open(inputs + '.json','r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
+loaded_model.load_weights(inputs + '.h5')
+print('model is loaded')
+"""
+    ----------------------------------------------------------------------------------
+"""
+
+
+
+# see https://keras.io/getting-started/faq/#how-can-i-obtain-the-output-of-an-intermediate-layer
+layer_name='dense_1'
+intermediate_layer_model = Model(input=loaded_model.input, \
+                        outputs=loaded_model.get_layer(layer_name).output)
+
+
+# calculate the 'finger prints' for cluster analysis
+finger_print=np.zeros((len(images),64))
+for i in len(images):
+    finger_print[i,:]=intermediate_layer_model.predict(images[i:i+1,:,:,0:1])
+    
+h5f = h5py.File(inputs + '_encoding.h5', 'w')
+h5f.create_dataset('encoding', data=finger_print)
+h5f.create_dataset('lens', data=lens)
+h5f.close()
+
+
