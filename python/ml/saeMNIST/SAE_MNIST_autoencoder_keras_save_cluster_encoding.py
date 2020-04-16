782aac38ce39d3d255bba7b594ea8c8feff9f7af
Paul Connolly <p.connolly@manchester.ac.uk>
2020-04-16 06:21:45 +0100

    bug fix

--- python/ml/saeMNIST/SAE_MNIST_autoencoder_keras_save_cluster_encoding.py
+++ python/ml/saeMNIST/SAE_MNIST_autoencoder_keras_save_cluster_encoding.py
@@ -32,8 +32,8 @@ if loadData:
     # load images
     (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
-    x_train=x_train.reshape((x_train.shape[0],-1))
-    x_test=x_test.reshape((x_test.shape[0],-1))
+    x_train=X_train.reshape((x_train.shape[0],-1))
+    x_test=X_test.reshape((x_test.shape[0],-1))
 
     x_train=x_train.astype('float32')/255.
     x_test=x_test.astype('float32')/255.
